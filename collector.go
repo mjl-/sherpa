dce9ce6ed361b90e42a49d0f68c6dc8669fabb34
Mechiel Lukkien <mechiel@ueber.net>
2018-11-23 12:18:33 +0100

    reformat comments for better godoc reading

--- collector.go
+++ collector.go
@@ -3,11 +3,15 @@ package sherpa
 // Collector facilitates collection of metrics. Functions are called by the library as such events or errors occur.
 // See https://github.com/irias/sherpa-prometheus-collector for an implementation for prometheus.
 type Collector interface {
-	ProtocolError()                                                              // When a request is invalid at protocol-level, e.g. wrong mimetype or request body.
-	BadFunction()                                                                // When called function does not exist.
-	JavaScript()                                                                 // When sherpa.js is requested.
-	JSON()                                                                       // When sherpa.json is requested.
-	FunctionCall(name string, error bool, serverError bool, durationSec float64) // When function "name" is called, and whether that caused an "error" at all, and a "serverError" in particular, and how long the call took.
+	ProtocolError() // Invalid request at protocol-level, e.g. wrong mimetype or request body.
+	BadFunction()   // Function does not exist.
+	JavaScript()    // Sherpa.js is requested.
+	JSON()          // Sherpa.json is requested.
+
+	// Function "name" is called, and whether that caused an
+	// "error" at all, and a "serverError" in particular, and how
+	// long the call took.
+	FunctionCall(name string, error bool, serverError bool, durationSec float64)
 }
 
 type ignoreCollector struct{}
